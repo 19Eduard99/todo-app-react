0a8f1e51f1f60a9583b65689e3fb1287407614b1
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2025-05-21 19:30:08 +0300

    create React Todo App

--- README.md
+++ README.md
@@ -1,9 +1,75 @@
-# React TypeScript Starter Pack
+# ToDo App (React + Typescript)
 
-To use this template click `Use this template`
+This is a simple ToDo App built with React and Typescript
 
-### Available Scripts
+# Live Demo
 
-`Deploy` - available to deploy your application to gh-pages, to deploy the project, change the second line of code in package.json, specifically the homepage value, from "." to the repository name
+[View Demo](https://19Eduard99.github.io/todo-app-react/)
 
-`SCSS Preprocessor` - available to write your styles with modern style language
+# Design
+
+Adaptive theme (light/dark), Minimalistic UI, and responsive design
+
+# Technologies Used
+
+### 🧱 Core
+
+- **React, Typescript, BlumaCSS + SCSS** — core structure, styling, and interactivity
+
+### 🎨 Design & UI/UX
+
+- **React-Transition-Group** — for smooth transitions
+
+### 🛠 Development & Deployment
+
+- **Vite** — build tool
+  **Stylelint** — CSS/SCSS linting
+- **ESLint** — JavaScript linting
+- **Prettier** — code formatting
+
+## Get Started
+
+Follow these steps to set up the project locally:
+
+### 1. Clone the repository
+
+```bash
+git clone https://github.com/19Eduard99/todo-app-react.git
+cd todo-app-react
+```
+
+### 2. Install dependencies
+
+Using npm:
+
+```bash
+npm install
+```
+
+or using yarn:
+
+```bash
+yarn install
+```
+
+### 3. Run the project locally
+
+Using npm:
+
+```bash
+npm start
+```
+
+or using yarn:
+
+```bash
+yarn start
+```
+
+## Features
+
+- Add, edit, and delete tasks
+- Mark tasks as completed
+- Filter tasks based on status (all, active, completed)
+- Clear completed tasks
+- Responsive design for different screen sizes
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,123 @@
-import React from 'react';
-import './App.scss';
+import React, { useEffect, useRef, useState } from 'react';
+import { getTodos } from './api/todos';
+import { Todo } from './types/Todo';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ErrorNotification from './components/ErrorNotification';
+import TodoList from './components/TodoList';
+
+export enum Filter {
+  All = 'All',
+  Active = 'Active',
+  Completed = 'Completed',
 }
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedNav, setSelectedNav] = useState(Filter.All);
+  const [activeTodoIds, setActiveTodoIds] = useState<number[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const loadTodos = async () => {
+    setError('');
+    try {
+      const res = await getTodos();
+
+      setTodos(res);
+    } catch (err) {
+      setError('Unable to load todos');
+    } finally {
+      inputRef.current?.focus();
+    }
+  };
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
+  useEffect(() => {
+    if (error) {
+      const timer = setTimeout(() => {
+        setError('');
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+
+    return;
+  }, [error]);
+
+  useEffect(() => {
+    if (tempTodo) {
+      setActiveTodoIds(current => [...current, tempTodo.id]);
+
+      if (tempTodo.id > 0) {
+        setTodos(current => [...current, tempTodo]);
+        setTempTodo(null);
+        setActiveTodoIds(current => current.filter(id => id !== tempTodo.id));
+      }
+    }
+  }, [tempTodo]);
+
+  const filteredTodos = todos.filter(todo => {
+    switch (selectedNav) {
+      case Filter.Active:
+        return !todo.completed;
+      case Filter.Completed:
+        return todo.completed;
+      default:
+        return true;
+    }
+  });
+
+  const handleFilter = (e: React.MouseEvent) => {
+    const filter =
+      e.currentTarget.getAttribute('href')?.replace('#', '') || Filter.All;
+
+    setSelectedNav(filter as Filter);
+  };
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+      <div className="todoapp__content">
+        <Header
+          todos={todos}
+          setTodos={setTodos}
+          inputRef={inputRef}
+          setTempTodo={setTempTodo}
+          setError={setError}
+          isLoading={isLoading}
+          setIsLoading={setIsLoading}
+          setActiveTodoIds={setActiveTodoIds}
+        />
+        <TodoList
+          filteredTodos={filteredTodos}
+          activeTodoIds={activeTodoIds}
+          setActiveTodoIds={setActiveTodoIds}
+          setTodos={setTodos}
+          tempTodo={tempTodo}
+          setError={setError}
+          inputRef={inputRef}
+        />
+        {todos.length > 0 && (
+          <Footer
+            todos={todos}
+            selectedNav={selectedNav}
+            handleFilter={handleFilter}
+            setError={setError}
+            inputRef={inputRef}
+            setTodos={setTodos}
+            isLoading={isLoading}
+            setActiveTodoIds={setActiveTodoIds}
+          />
+        )}
+      </div>
+      <ErrorNotification error={error} setError={setError} />
     </div>
   );
 };
